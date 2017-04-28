c8edc16e2c61f957e4473867bc219534602d371b
Christophe SAUVEUR <christophe@cheeseburgames.com>
2017-04-28 08:31:19 +0200

    DatabaseUpdater: Changed updater prefix for updater domain terminology

--- framework/classes/DatabaseUpdater.php
+++ framework/classes/DatabaseUpdater.php
@@ -4,20 +4,20 @@ namespace CheeseBurgames\MFX;
 final class DatabaseUpdater implements IRouteProvider {
 	
 	private static $_updatersData = NULL;
-	private static $_updatersPrefix = NULL;
+	private static $_updatersDomain = NULL;
 	
 	/**
 	 * @mfx_subroute
 	 */
 	public static function update() {
-		$updaters = array_merge(array( FrameworkDatabaseUpdater::class ), Config::get('database.updaters', array()));
+		$updaters = array_merge(array( FrameworkDatabaseUpdater::class ), Config::get('database.updaters.classes', array()));
 		if (!is_array($updaters) || empty($updaters))
 			return;
 		
-		// Retrieving updaters prefix
-		self::$_updatersPrefix = Config::get('database.updaters_prefix', NULL);
-		if (!preg_match('/^[[:alnum:]_-]+$/', self::$_updatersPrefix))
-			self::$_updatersPrefix = NULL;
+		// Retrieving updaters domain
+		self::$_updatersDomain = Config::get('database.updaters.domain', NULL);
+		if (!preg_match('/^[[:alnum:]_-]+$/', self::$_updatersDomain))
+			self::$_updatersDomain = NULL;
 		
 		// Initializing database manager
 		$dbm = DatabaseManager::open('__mfx');
@@ -25,20 +25,20 @@ final class DatabaseUpdater implements IRouteProvider {
 		// Creating updaters table
 		$dbm->exec("CREATE TABLE IF NOT EXISTS `mfx_database_updaters` (
 					`updater_key` varchar(255) COLLATE utf8_bin NOT NULL,
-					`updater_prefix` varchar(255) COLLATE utf8_bin NULL DEFAULT NULL,
+					`updater_domain` varchar(255) COLLATE utf8_bin NULL DEFAULT NULL,
 					`updater_version` smallint(5) unsigned NOT NULL,
 					`updater_file_modified` TIMESTAMP NULL DEFAULT NULL,
-					PRIMARY KEY (`updater_key`, `updater_prefix`)
+					PRIMARY KEY (`updater_key`, `updater_domain`)
 		)");
 		
 		// Load versions and file modification times
 		$sql = "SELECT `updater_key`, `updater_version`, UNIX_TIMESTAMP(`updater_file_modified`) AS `updater_filemtime`
 					FROM `mfx_database_updaters`";
-		if (self::$_updatersPrefix === NULL)
-			$sql .= " WHERE `updater_prefix` IS NULL";
+		if (self::$_updatersDomain === NULL)
+			$sql .= " WHERE `updater_domain` IS NULL";
 		else
-			$sql .= " WHERE `updater_prefix` = ?";
-		self::$_updatersData = $dbm->getIndexed($sql, 'updater_key', \PDO::FETCH_OBJ, self::$_updatersPrefix);
+			$sql .= " WHERE `updater_domain` = ?";
+		self::$_updatersData = $dbm->getIndexed($sql, 'updater_key', \PDO::FETCH_OBJ, self::$_updatersDomain);
 		
 		foreach ($updaters as $updater) {
 			$rc = new \ReflectionClass($updater);
@@ -105,10 +105,10 @@ final class DatabaseUpdater implements IRouteProvider {
 					}
 				}
 				
-				$sql = "INSERT INTO `mfx_database_updaters` (`updater_key`, `updater_prefix`, `updater_version`, `updater_file_modified`)
+				$sql = "INSERT INTO `mfx_database_updaters` (`updater_key`, `updater_domain`, `updater_version`, `updater_file_modified`)
 							VALUE (?, ?, ?, FROM_UNIXTIME(?))
 							ON DUPLICATE KEY UPDATE `updater_version` = VALUES(`updater_version`), `updater_file_modified` = VALUES(`updater_file_modified`)";
-				if ($dbmMFX->exec($sql, $key, self::$_updatersPrefix, $version, $mtime) === false) {
+				if ($dbmMFX->exec($sql, $key, self::$_updatersDomain, $version, $mtime) === false) {
 					trigger_error(sprintf(dgettext('mfx', "An error has occured while processing DatabaseUpdater '%s'."), $key), E_USER_ERROR);
 					return false;
 				}
