ffd83a1b2ae06f4085a04d51fba86bf125998bf1
Christophe SAUVEUR <christophe@cheeseburgames.com>
2017-04-30 18:39:19 +0200

    DataValidator: - Added IsOfType filter to validate variable type

---
+++ framework/classes/DataValidator/Filter/IsOfType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Data validation "value is of type" filter class
+ *
+ * @author Christophe SAUVEUR <christophe@cheeseburgames.com>
+ * @version 1.0
+ * @package framework
+ */
+
+namespace CheeseBurgames\MFX\DataValidator\Filter;
+
+use CheeseBurgames\MFX\DataValidator\AbstractFilter;
+
+/**
+ * Description of a filter validating if the specified value is of a specific type
+ */
+class IsOfType extends AbstractFilter {
+	
+	/**
+	 * @var string Variable type
+	 */
+	private $_type;
+	
+	/**
+	 * Constructor
+	 * @param string $type Variable type
+	 * @param string $message Error message (Defaults to NULL)
+	 * @throws \InvalidArgumentException
+	 */
+	public function __construct($type, string $message = NULL) {
+		if (!in_array($type, array( 'boolean', 'integer', 'double', 'string', 'array', 'object', 'resource', 'NULL' )))
+			throw new \InvalidArgumentException("type argument must be one of 'boolean', 'integer', 'double', 'string', 'array', 'object', 'resource' or 'NULL'");
+		$this->_type = $type;
+			
+		if (empty($message))
+			$message = sprintf(dgettext('mfx', "The '%%s' field value must be of type '%s'."), $type);
+		parent::__construct ( $message = NULL );
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \CheeseBurgames\MFX\DataValidator\AbstractFilter::validate()
+	 */
+	public function validate($fieldName, $value, $atIndex = NULL, $silent = false) {
+		if (gettype($value) != $this->_type) {
+			if (empty($silent))
+				$this->emitMessage($fieldName);
+			return false;
+		}
+		else
+			return true;
+	}
+}
