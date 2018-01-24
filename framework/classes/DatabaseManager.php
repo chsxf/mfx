4813c4649a6845b1c74a51fd5eec61c50460ccf6
Christophe SAUVEUR <christophe@cheeseburgames.com>
2018-01-24 16:34:45 +0100

    DatabaseManager - Added error login support through configuration option

--- framework/classes/DatabaseManager.php
+++ framework/classes/DatabaseManager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Database management helpers
- * 
+ *
  * @author Christophe SAUVEUR <christophe@cheeseburgames.com>
  * @version 1.0
  * @package framework
@@ -20,20 +20,20 @@ final class DatabaseManager extends \CheeseBurgames\PDO\DatabaseManager
 	 * @var array Open connections container
 	 */
 	private static $_openConnections = array();
-	
+
 	/**
 	 * Constructor
 	 * @param string $dsn Data Source Name (ie mysql:host=localhost;dbname=mydb)
 	 * @param string $username Username
 	 * @param string $password Password
-	 * 
+	 *
 	 * @see \PDO::__construct()
 	 */
 	public function __construct($dsn, $username, $password)
 	{
-		parent::__construct($dsn, $username, $password);
+		parent::__construct($dsn, $username, $password, Config::get('database.error_logging', false));
 	}
-	
+
 	/**
 	 * Opens a connection to a database server, or returns the currently active connection to this server
 	 * @param string $server Server configuration key (Defaults to __default).
@@ -45,27 +45,27 @@ final class DatabaseManager extends \CheeseBurgames\PDO\DatabaseManager
 	{
 		if (array_key_exists($server, self::$_openConnections) && empty($forceNew))
 			return self::$_openConnections[$server];
-		
+
 		if (!Config::has('database.servers'))
 			throw new DatabaseManagerException("No database server configured.");
-		
+
 		$serverConfig = Config::get("database.servers.{$server}");
 		if (is_string($serverConfig) && !empty($serverConfig))
 			$serverConfig = Config::get("database.servers.{$serverConfig}");
 		if (empty($serverConfig) || !is_array($serverConfig))
 			throw new DatabaseManagerException("No server can be found for the '{$server}' key.");
-		
+
 		foreach (array('dsn', 'username', 'password') as $p)
 		{
 			if (empty($serverConfig[$p]))
 				throw new DatabaseManagerException("Unable to find the '{$p}' parameter for database server '{$server}'.");
 			$$p = $serverConfig[$p];
 		}
-		
+
 		$dbm = new DatabaseManager($dsn, $username, $password);
 		if (!array_key_exists($server, self::$_openConnections))
 			self::$_openConnections[$server] = $dbm;
 		return $dbm;
 	}
-	
+
 }
