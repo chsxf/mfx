a68517bb9d002fe7e32b90e7c71bb1159077c316
Christophe SAUVEUR <christophe@cheeseburgames.com>
2018-06-29 14:01:33 +0200

    Fixed a bug in the User::registerFromFields() function in the SQL generator

--- framework/classes/User.php
+++ framework/classes/User.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * User descriptor
- * 
+ *
  * @author Christophe SAUVEUR <christophe@cheeseburgames.com>
  * @version 1.0
  * @package framework
@@ -18,17 +18,17 @@ class User
 	 * @var User Current registered user reference
 	 */
 	private static $_currentUser;
-	
+
 	/**
 	 * @var boolean If set, the current registered is a valid user. Either, the user is a guest
 	 */
 	private $_valid;
-	
+
 	/**
 	 * @var string User key. NULL for guests and most commonly the user database ID for valid users.
 	 */
 	private $_key;
-	
+
 	/**
 	 * @var array User data fetched from the database
 	 */
@@ -37,7 +37,7 @@ class User
 	 * @var boolean If set, user data has been fetched.
 	 */
 	private $_dataFetched;
-	
+
 	/**
 	 * Validates user session
 	 */
@@ -45,17 +45,17 @@ class User
 		// Authenticator class
 		$rc = new \ReflectionClass(Config::get('user_management.class', __CLASS__));
 		self::$_currentUser = $rc->newInstance();
-		
+
 		// Validating
 		if (!empty($_SESSION['logged_user']))
 		{
 			list($key, $ip) = explode('|', $_SESSION['logged_user'], 2);
-	
+
 			if ($ip != $_SERVER['REMOTE_ADDR'] || !self::$_currentUser->registerFromKey($key))
 				unset($_SESSION['logged_user']);
 		}
 	}
-	
+
 	/**
 	 * Validates a user session using database fields
 	 * @param array $fields Key-value pairs for database validation
@@ -70,7 +70,7 @@ class User
 		else
 			return false;
 	}
-	
+
 	/**
 	 * Sets in session the current user's key if not already set
 	 * @param string $key Current user's key
@@ -79,14 +79,14 @@ class User
 		if (!isset($_SESSION['logged_user']))
 			$_SESSION['logged_user'] = sprintf("%s|%s", $key, $_SERVER['REMOTE_ADDR']);
 	}
-	
+
 	/**
 	 * Invalidates user session. Logs out the current valid user if existing
 	 */
 	public static function invalidate() {
 		unset($_SESSION['logged_user']);
 	}
-	
+
 	/**
 	 * Gets the current user reference
 	 * @return User
@@ -94,27 +94,27 @@ class User
 	public static function currentUser() {
 		return self::$_currentUser;
 	}
-	
+
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param string $key User key
 	 */
 	public function __construct($key = NULL) {
 		$this->_valid = false;
 		$this->_key = NULL;
-			
+
 		$this->_data = NULL;
 		$this->_dataFetched = false;
-		
+
 		if ($key !== NULL)
 			$this->registerFromKey($key);
 	}
-	
+
 	/**
 	 * Retrieves users management key field name
 	 * @throws \InvalidArgumentException If the provided value is not a string or contains invalid characters (only underscores and alphanumeric characters are accepted)
-	 * @return string 
+	 * @return string
 	 */
 	public static function getKeyField() {
 		$keyFieldName = Config::get('user_management.key_field', 'user_id');
@@ -124,7 +124,7 @@ class User
 			throw new \InvalidArgumentException("Users management key field name contains invalid characters (only underscores and alphanumeric characters are accepted).");
 		return $keyFieldName;
 	}
-	
+
 	/**
 	 * Retrieves users management table name
 	 * @throws \InvalidArgumentException If the provided value is not a string or contains invalid characters (only underscores and alphanumeric characters are accepted)
@@ -138,7 +138,7 @@ class User
 			throw new \InvalidArgumentException("Users management table name contains invalid characters (only underscores and alphanumeric characters are accepted).");
 		return $tableName;
 	}
-	
+
 	/**
 	 * Register a user from database fields
 	 * @param array $fields Database fields used to identify the user
@@ -147,7 +147,7 @@ class User
 	public function registerWithFields(array $fields) {
 		if (empty($fields))
 			return false;
-		
+
 		$sql = sprintf("SELECT `%s` FROM `%s` WHERE ", self::getKeyField(), self::getTableName());
 		$validFields = array();
 		$values = array();
@@ -165,7 +165,7 @@ class User
 			else
 				$str .= empty($f['operator']) ? ' = ' : $f['operator'];
 			if (!empty($f['function'])) {
-				if (strpos($f['function'], '(') < 0)
+				if (strpos($f['function'], '(') === false)
 					$str .= "{$f['function']}(?)";
 				else
 					$str .= $f['function'];
@@ -173,28 +173,28 @@ class User
 			else
 				$str .= '?';
 			$validFields[] = $str;
-	
+
 			$values[] = $f['value'];
 		}
 		$sql .= implode(' AND ', $validFields) . ' LIMIT 1';
 		array_unshift($values, $sql);
-		
+
 		$dbm = DatabaseManager::open("__mfx");
 		$key = call_user_func_array(array(&$dbm, 'getValue'), $values);
 		$dbm = NULL;
 		if ($key === false)
 			return false;
-		
+
 		$this->_key = $key;
 		$this->_valid = true;
 		return true;
 	}
-	
+
 	/**
 	 * Validates the user key
 	 * @param string $key User key to validate
 	 * @return boolean true if the user key is valid, false either
-	 * 
+	 *
 	 * @used-by User::registerFromKey()
 	 */
 	protected function _validateKey($key) {
@@ -202,12 +202,12 @@ class User
 		$nb = $dbm->getValue(sprintf('SELECT COUNT(`%1$s`) FROM `%2$s` WHERE `%1$s` = ?', self::getKeyField(), self::getTableName()), $key);
 		return !empty($nb);
 	}
-	
+
 	/**
 	 * Register a user from its key
 	 * @param string $key User key
 	 * @return boolean true is the key is valid, false either
-	 * 
+	 *
 	 * @uses User::_validateKey()
 	 */
 	public function registerFromKey($key) {
@@ -220,7 +220,7 @@ class User
 		else
 			return false;
 	}
-	
+
 	/**
 	 * Gets the current user key
 	 * @return string The function returns NULL if no valid user is currently registered
@@ -228,7 +228,7 @@ class User
 	public function getKey() {
 		return $this->_key;
 	}
-	
+
 	/**
 	 * Gets the current user status.
 	 * @return boolean true if the current user is valid, false for guests
@@ -236,7 +236,7 @@ class User
 	public function isValid() {
 		return $this->_valid;
 	}
-	
+
 	/**
 	 * Fetches user data from the database
 	 * @return boolean true if data has been successfully fetched, false either
@@ -244,7 +244,7 @@ class User
 	protected final function fetch() {
 		if (!$this->isValid())
 			return false;
-	
+
 		if ($this->_dataFetched == false)
 		{
 			if (($data = $this->fetchData()) === false)
@@ -252,14 +252,14 @@ class User
 				$this->_valid = false;
 				return false;
 			}
-				
+
 			$this->_data = $data;
 			$this->_dataFetched = true;
 		}
-			
+
 		return true;
 	}
-	
+
 	/**
 	 * Fetches user data from the database.
 	 * This function can be overridden.
@@ -271,12 +271,12 @@ class User
 		$dbm = NULL;
 		return $row;
 	}
-	
+
 	/**
 	 * PHP magic method
 	 * @param string $name Variable name
 	 * @return mixed
-	 * 
+	 *
 	 * @link http://www.php.net/manual/en/language.oop5.magic.php
 	 */
 	public function __get($name) {
@@ -284,12 +284,12 @@ class User
 			return NULL;
 		return array_key_exists($name, $this->_data) ? $this->_data[$name] : NULL;
 	}
-	
+
 	/**
 	 * PHP magic method
 	 * @param string $name Varible name
 	 * @return boolean
-	 * 
+	 *
 	 * @link http://www.php.net/manual/en/language.oop5.magic.php
 	 */
 	public function __isset($name) {
