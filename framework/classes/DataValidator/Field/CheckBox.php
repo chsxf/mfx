d3bc20b64508626729b4eb0164622dff3aa4f404
Christophe SAUVEUR <christophe@cheeseburgames.com>
2018-12-21 15:15:13 +0100

    DataValidator: Improved checkbox behaviour to match other field types behaviour more closely

--- framework/classes/DataValidator/Field/CheckBox.php
+++ framework/classes/DataValidator/Field/CheckBox.php
@@ -1,12 +1,11 @@
 <?php
 /**
  * Data validation Checkbox field type class
- * 
+ *
  * @author Christophe SAUVEUR <christophe@cheeseburgames.com>
  * @version 1.0
  * @package framework
  */
-
 namespace CheeseBurgames\MFX\DataValidator\Field;
 
 use CheeseBurgames\MFX\DataValidator\Field;
@@ -15,21 +14,11 @@ use CheeseBurgames\MFX\DataValidator\FieldType;
 /**
  * Descriptor of a checkbox field type
  */
-class CheckBox extends Field
-{
-	/**
-	 * Constructor
-	 * @param string $name Field's name
-	 * @param FieldType $type Field's type
-	 * @param mixed $defaultValue Field's default value
-	 * @param boolean $required If set, this field will become required in the validation process.
-	 */
-	protected function __construct($name, FieldType $type, $defaultValue, $required) {
-		parent::__construct($name, $type, empty($defaultValue) ? 0 : $defaultValue, $required);
-	}
-	
+class CheckBox extends Field {
+
 	/**
 	 * (non-PHPdoc)
+	 *
 	 * @see Field::generate()
 	 * @param array $containingGroups
 	 * @param FieldType $type_override
@@ -41,35 +30,16 @@ class CheckBox extends Field
 		$result[1]['value'] = 1;
 		return $result;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * @see Field::getValue()
-	 * 
-	 * @param bool $returnDefaultIfNotSet
-	 */
-	public function getValue($returnDefaultIfNotSet = false) {
-		return parent::getValue(true);
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see Field::getIndexedValue()
-	 * 
-	 * @param int $index
-	 * @param bool $returnDefaultIfNotSet
-	 */
-	public function getIndexedValue($index, $returnDefaultIfNotSet = false) {
-		return parent::getIndexedValue($index, true);
-	}
-	
-	/**
-	 * (non-PHPdoc)
+	 *
 	 * @see Field::revertToDefaultIfNotPopulated()
 	 */
 	public function revertToDefaultIfNotPopulated() {
 		return $this->isEnabled();
 	}
+
 }
 
 FieldType::registerClassForType(new FieldType(FieldType::CHECKBOX), CheckBox::class);
