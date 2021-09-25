93599aaa160a2559a1472903d92bdf96eb966de3
Christophe SAUVEUR <chsxf.pro@gmail.com>
2021-09-26 01:06:53 +0200

    🎨 Updated XMLTools.php to PHP 8 standards

--- src/chsxf/MFX/XMLTools.php
+++ src/chsxf/MFX/XMLTools.php
@@ -15,15 +15,15 @@ class XMLTools
 	/**
 	 * @var string Stores the string representation of PHP_INT_MAX
 	 */
-	private static $PHP_INT_MAX_AS_STR = NULL;
+	private static ?string $PHP_INT_MAX_AS_STR = NULL;
 	/**
 	 * @var int Stores the string representation length of PHP_INT_MAX
 	 */
-	private static $PHP_INT_MAX_LENGTH = 0;
+	private static int $PHP_INT_MAX_LENGTH = 0;
 	/**
 	 * @var array Container for object references used to avoid recursions
 	 */
-	private static $RECURSIONS;
+	private static array $RECURSIONS;
 	
 	/**
 	 * Write XML tree from a variable
@@ -35,59 +35,58 @@ class XMLTools
 	 *
 	 * @used-by XMLTools::build()
 	 */
-	private static function _build(\XMLWriter $writer, $var, $filterStrings = true)
-	{
+	private static function _build(\XMLWriter $writer, mixed $var, bool $filterStrings = true) {
 		// NULL
-		if (is_null($var))
-			$writer->writeElement('null');
+        if (is_null($var)) {
+            $writer->writeElement('null');
+        }
 		// Scalar values
-		else if (is_scalar($var))
-		{
-			if (is_string($var))
-			{
+		else if (is_scalar($var)) {
+			if (is_string($var)) {
 				$regs = NULL;
 				
 				// Booleans as string
-				if ($filterStrings && preg_match('/^(true|false)$/', $var))
-					$writer->writeElement('bool', $var);
+                if ($filterStrings && preg_match('/^(true|false)$/', $var)) {
+                    $writer->writeElement('bool', $var);
+                }
 				// Integers as string
-				else if ($filterStrings && preg_match('/^-?([1-9]\d*)$/', $var, $regs))
-				{
-					if (self::$PHP_INT_MAX_AS_STR === NULL)
-					{
+				else if ($filterStrings && preg_match('/^-?([1-9]\d*)$/', $var, $regs)) {
+					if (self::$PHP_INT_MAX_AS_STR === NULL) {
 						self::$PHP_INT_MAX_AS_STR = strval(PHP_INT_MAX);
 						self::$PHP_INT_MAX_LENGTH = strlen(self::$PHP_INT_MAX_AS_STR);
 					}
 	
 					$length = strlen($regs[1]);
-					if ($length < self::$PHP_INT_MAX_LENGTH || ($length == self::$PHP_INT_MAX_LENGTH && strcmp(self::$PHP_INT_MAX_AS_STR, $regs[1]) >= 0))
-						$writer->writeElement('int', $var);
-					else
-					{
+                    if ($length < self::$PHP_INT_MAX_LENGTH || ($length == self::$PHP_INT_MAX_LENGTH && strcmp(self::$PHP_INT_MAX_AS_STR, $regs[1]) >= 0)) {
+                        $writer->writeElement('int', $var);
+                    }
+					else {
 						$writer->startElement('string');
 						$writer->writeCdata($var);
 						$writer->endElement();
 					}
 				}
-				else if ($filterStrings && preg_match('/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/', $var))
+				else if ($filterStrings && preg_match('/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/', $var)) {
 					$writer->writeElement('float', $var);
-				else
-				{
+				}
+				else {
 					$writer->startElement('string');
 					$writer->writeCdata($var);
 					$writer->endElement();
 				}
 			}
-			else if (is_int($var))
+			else if (is_int($var)) {
 				$writer->writeElement('int', strval($var));
-			else if (is_float($var))
+			}
+			else if (is_float($var)) {
 				$writer->writeElement('float', strval($var));
-			else if (is_bool($var))
+			}
+			else if (is_bool($var)) {
 				$writer->writeElement('bool', $var ? 'true' : 'false');
+			}
 		}
 		// Arrays
-		else if (is_array($var))
-		{
+		else if (is_array($var)) {
 			$writer->startElement('array');
 			foreach ($var as $k => $v) {
 				$writer->startElement('key');
@@ -98,11 +97,9 @@ class XMLTools
 			$writer->endElement();
 		}
 		// Objects
-		else if (is_object($var))
-		{
+		else if (is_object($var)) {
 			// Check for recursions
-			if (in_array($var, self::$RECURSIONS))
-			{
+			if (in_array($var, self::$RECURSIONS)) {
 				$writer->startElement('object');
 				$writer->writeAttribute('recursion', 'true');
 				$writer->endElement();
@@ -114,11 +111,11 @@ class XMLTools
 			
 			$ro = new \ReflectionObject($var);
 			$writer->startElement('object');
-			if ($var instanceof \stdClass == false)
-				$writer->writeAttribute('class', $ro->name);
+            if ($var instanceof \stdClass == false) {
+                $writer->writeAttribute('class', $ro->name);
+            }
 			$props = $ro->getProperties(\ReflectionProperty::IS_PUBLIC);
-			foreach ($props as $v)
-			{
+			foreach ($props as $v) {
 				$writer->startElement('prop');
 				$writer->writeCdata($v->getName());
 				$writer->endElement();
@@ -136,7 +133,7 @@ class XMLTools
 	 * @param string $encoding Encoding charset (Defaults to UTF-8). 
 	 * @return string the XML tree string
 	 */
-	public static function build($var, $encoding = 'UTF-8') {
+	public static function build(mixed $var, string $encoding = 'UTF-8'): string {
 		self::$RECURSIONS = array();
 		
 		$writer = new \XMLWriter();
