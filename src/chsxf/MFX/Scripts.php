424c961ba3f89c2de50473c60c58e4a36b19ba31
Christophe SAUVEUR <chsxf.pro@gmail.com>
2021-09-25 18:34:37 +0200

    🎨 Updated Scripts.php to PHP 8 standards

--- src/chsxf/MFX/Scripts.php
+++ src/chsxf/MFX/Scripts.php
@@ -22,7 +22,7 @@ class Scripts
 	/**
 	 * @var array Scripts container
 	 */
-	private static $scripts = array();
+	private static array $scripts = array();
 	
 	/**
 	 * Adds a script to the document
@@ -32,19 +32,19 @@ class Scripts
 	 * @param string $type Script type (Defaults to text/javascript).
 	 * @throws ScriptException If the URL is empty, or if the file does not exists or is not readable for inline scripts.
 	 */
-	public static function add($url, $inline = false, $prepend = false, $type = 'text/javascript') {
-		if (empty($url))
-			throw new ScriptException("'{$url} is not a valid script URL.");
+	public static function add(string $url, bool $inline = false, bool $prepend = false, string $type = 'text/javascript') {
+        if (empty($url)) {
+            throw new ScriptException("'{$url} is not a valid script URL.");
+        }
 		
 		$url = CoreManager::convertFakeProtocols($url);
-		if (!empty($inline) && (!file_exists($url) || !is_file($url) || !is_readable($url)))
-			throw new ScriptException("'{$url} is not a valid script URL.");
+        if (!empty($inline) && (!file_exists($url) || !is_file($url) || !is_readable($url))) {
+            throw new ScriptException("'{$url} is not a valid script URL.");
+        }
 		
-		if (empty($inline) && !Config::isGoogleAppEngineRuntime() && !preg_match('#^(.+:)?//#', $url) && strtolower(pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION)) == 'js')
-		{
+		if (empty($inline) && !preg_match('#^(.+:)?//#', $url) && strtolower(pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION)) == 'js') {
 			$regs = NULL;
-			if (preg_match('/^(.+)\.(\w+)$/', $url, $regs) && file_exists($url))
-			{
+			if (preg_match('/^(.+)\.(\w+)$/', $url, $regs) && file_exists($url)) {
 				$mtime = filemtime($url);
 				$url = sprintf("%s_%d.%s", $regs[1], $mtime, $regs[2]);
 			}
@@ -56,10 +56,12 @@ class Scripts
 			'type' => $type,
 			'content' => empty($inline) ? NULL : file_get_contents($url)
 		);
-		if ($prepend)
-			array_unshift(self::$scripts, $obj);
-		else
-			self::$scripts[] = $obj;
+        if ($prepend) {
+            array_unshift(self::$scripts, $obj);
+        }
+		else {
+            self::$scripts[] = $obj;
+        }
 	}
 	
 	/**
@@ -67,11 +69,11 @@ class Scripts
 	 * @param \Twig_Environment $twig Twig environnement used for rendering HTML
 	 * @return string
 	 */
-	public static function export(Environment $twig) {
-		foreach (self::$scripts as &$v)
-		{
-			if ($v->inline)
-				$v->content = $twig->render($v->content);
+	public static function export(Environment $twig): string {
+		foreach (self::$scripts as &$v) {
+            if ($v->inline) {
+                $v->content = $twig->render($v->content);
+            }
 		}
 		return $twig->render('@mfx/Scripts.twig', array( 'scripts' => self::$scripts ));
 	}
