8af39bf6f3292b57abee4e5e8cdfaf4722cf1657
Christophe SAUVEUR <chsxf.pro@gmail.com>
2021-09-26 00:59:36 +0200

    🎨 Updated StyleSheets.php to PHP 8 standards

--- src/chsxf/MFX/StyleSheets.php
+++ src/chsxf/MFX/StyleSheets.php
@@ -22,28 +22,27 @@ class StyleSheets
 	/**
 	 * @var array Style sheets container
 	 */
-	private static $styleSheets = array();
+	private static array $styleSheets = array();
 	
 	/**
 	 * Adds a style sheets to the document
 	 * @param string $url Style sheet URL or path for inline sheets
 	 * @param string $media Media type (Defaults to screen)
-	 * @param string $inline If set, the style sheet is included inline in the response (Defaults to false).
-	 * @param string $prepend If set, the style sheet is added before any other (Defaults to false).
+	 * @param bool $inline If set, the style sheet is included inline in the response (Defaults to false).
+	 * @param bool $prepend If set, the style sheet is added before any other (Defaults to false).
 	 * @param string $type Style sheet type (Defaults to text/css).
 	 * @throws StyleSheetException If the URL is empty, or if the file does not exists or is not readable for inline sheets.
 	 */
-	public static function add($url, $media = 'screen', $inline = false, $prepend = false, $type = 'text/css')
+	public static function add(string $url, string $media = 'screen', bool $inline = false, bool $prepend = false, string $type = 'text/css')
 	{
-		if (empty($url) || (!empty($inline) && (!file_exists($url) || !is_file($url) || !is_readable($url))))
-			throw new StyleSheetException("'{$url} is not a valid style sheet URL.");
+        if (empty($url) || (!empty($inline) && (!file_exists($url) || !is_file($url) || !is_readable($url)))) {
+            throw new StyleSheetException("'{$url} is not a valid style sheet URL.");
+        }
 
 		$url = CoreManager::convertFakeProtocols($url);
-		if (empty($inline) && !Config::isGoogleAppEngineRuntime() && !preg_match('#^(.+:)?//#', $url))
-		{
+		if (empty($inline) && !preg_match('#^(.+:)?//#', $url)) {
 			$regs = NULL;
-			if (preg_match('/^(.+)\.(\w+)$/', $url, $regs) && file_exists($url))
-			{
+			if (preg_match('/^(.+)\.(\w+)$/', $url, $regs) && file_exists($url)) {
 				$mtime = filemtime($url);
 				$url = sprintf("%s_%d.%s", $regs[1], $mtime, $regs[2]);
 			}
@@ -69,7 +68,7 @@ class StyleSheets
 	 * @param \Twig_Environment $twig Twig environnement used for rendering HTML
 	 * @return string
 	 */
-	public static function export(Environment $twig) {
+	public static function export(Environment $twig): string {
 		return $twig->render('@mfx/StyleSheets.twig', array( 'sheets' => self::$styleSheets ));
 	}
 }
