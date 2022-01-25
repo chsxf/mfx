9689c38a3f59d0dcbb78a9f5994823dee92264d3
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-01-25 13:07:12 +0100

    ♻️ Added support of PHP 8.1 native enumerations to RequiredRequestMethodAttribute class

--- src/chsxf/MFX/Attributes/RequiredRequestMethodAttribute.php
+++ src/chsxf/MFX/Attributes/RequiredRequestMethodAttribute.php
@@ -1,41 +1,13 @@
 <?php
 namespace chsxf\MFX\Attributes;
 
-use Error;
-use ReflectionClass;
-use ReflectionClassConstant;
 use Attribute;
+use chsxf\MFX\RequestMethod;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class RequiredRequestMethodAttribute extends AbstractRouteStringAttribute
 {
-    public const GET = 'GET';
-    public const HEAD = 'HEAD';
-    public const POST = 'POST';
-    public const PUT = 'PUT';
-    public const DELETE = 'DELETE';
-    public const CONNECT = 'CONNECT';
-    public const OPTIONS = 'OPTIONS';
-    public const TRACE = 'TRACE';
-    public const PATCH = 'PATCH';
-
-    private static ?array $classConstants = NULL;
-
-    public function __construct(string $value) {
-        if (self::$classConstants === null) {
-            self::$classConstants = array();
-
-            $rc = new ReflectionClass(__CLASS__);
-            $constants = $rc->getConstants(ReflectionClassConstant::IS_PUBLIC);
-            foreach ($constants as $constant) {
-                self::$classConstants[] = $constant->getValue();
-            }
-        }
-
-        if (!in_array($value, self::$classConstants, true)) {
-            throw new Error("Invalid request method '{$value}'");
-        }
-
-        parent::__construct($value);
+    public function __construct(RequestMethod $_requestMethod) {
+        parent::__construct($_requestMethod->name);
     }
 }
---
+++ src/chsxf/MFX/RequestMethod.php
@@ -0,0 +1,15 @@
+<?php
+namespace chsxf\MFX;
+
+enum RequestMethod
+{
+    case GET;
+    case HEAD;
+    case POST;
+    case PUT;
+    case DELETE;
+    case CONNECT;
+    case OPTIONS;
+    case TRACE;
+    case PATCH;
+}
