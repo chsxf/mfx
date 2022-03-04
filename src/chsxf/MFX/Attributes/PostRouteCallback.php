f5e78f26f1506a5bd8ff22b8bc6cbdf63051f661
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-03-04 23:18:19 +0100

    🐛 Ensured post-route callbacks are always called after each request

---
+++ src/chsxf/MFX/Attributes/PostRouteCallback.php
@@ -0,0 +1,17 @@
+<?php
+namespace chsxf\MFX\Attributes;
+
+use Error;
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
+class PostRouteCallback extends AbstractRouteStringAttribute
+{
+    public function __construct(string $value) {
+        if (!is_callable($value)) {
+            throw new Error("Invalid callable '{$value}'");
+        }
+
+        parent::__construct($value);
+    }
+}
